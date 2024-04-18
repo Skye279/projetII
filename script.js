c34fb5f3b4baeb8d91c0864a31872762241f5dca
Skye IA <imeneameur6@gmail.com>
2024-04-18 12:26:33 +0200

    up

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-function addTask() {
-    var inputField = document.getElementById("taskInput");
-    var taskList = document.getElementById("taskList");
-    var taskText = inputField.value;
-
-    if (taskText.trim() === "") {
-        alert("Please enter a task!");
-        return;
-    }
-
-    var listItem = document.createElement("li");
-    listItem.textContent = taskText;
-
-    taskList.appendChild(listItem);
-    inputField.value = "" ;}
+function addTask(){
+    console.log(document)
+    const taskinput= document.querySelector('#taskinput');
+    let taskvalue= taskinput.value
+    console.log(taskvalue)
+}
